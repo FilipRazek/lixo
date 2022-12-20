067a63b148147240e9d5cb25156e291fcbbd6427
FilipRazek <65445960+FilipRazek@users.noreply.github.com>
2022-12-20 21:40:24 +0100

    Initialize backend with Spring app

---
+++ backend/src/main/java/com/filiprazek/lixo/LixoApplication.java
@@ -0,0 +1,13 @@
+package com.filiprazek.lixo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LixoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LixoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/filiprazek/lixo/LixoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filiprazek.lixo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LixoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
