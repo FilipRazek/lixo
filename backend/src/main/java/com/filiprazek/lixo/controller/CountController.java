90c9356eaecb56d9fe0e4f8d4d3fcd7bd94dc048
FilipRazek <65445960+FilipRazek@users.noreply.github.com>
2022-12-21 21:08:42 +0100

    Add POST /count endpoint

--- backend/src/main/java/com/filiprazek/lixo/controller/CountController.java
+++ backend/src/main/java/com/filiprazek/lixo/controller/CountController.java
@@ -1,14 +1,28 @@
 package com.filiprazek.lixo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("count")
 @CrossOrigin(origins = "*")
 public class CountController {
-    @GetMapping("/getcount")
-    public int getCount() {
-        return 364;
-    }
+
+  Logger logger = Logger.getLogger(CountController.class.getName());
+
+  @GetMapping
+  public String getCount() {
+    return "364";
+  }
+
+  @PostMapping
+  public void setCount(@RequestBody String newCount) {
+    logger.log(Level.INFO, newCount);
+  }
 }
