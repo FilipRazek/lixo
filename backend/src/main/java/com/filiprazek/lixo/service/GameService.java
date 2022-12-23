a55e26784feee6bc90cf9d29be6e81b54e942d58
FilipRazek <65445960+FilipRazek@users.noreply.github.com>
2022-12-23 12:58:44 +0100

    Implement xo move logic

--- backend/src/main/java/com/filiprazek/lixo/service/GameService.java
+++ backend/src/main/java/com/filiprazek/lixo/service/GameService.java
@@ -2,7 +2,12 @@ package com.filiprazek.lixo.service;
 
 import com.filiprazek.lixo.data.Game;
 import com.filiprazek.lixo.exception.EntityNotFoundException;
+import com.filiprazek.lixo.exception.InvalidMoveException;
 import com.filiprazek.lixo.repository.GameRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,7 +31,28 @@ public class GameService {
 
     public Game move(String id, int move) {
         Game game = this.findById(id);
-        game.setBoard(move);
+        int board = game.getBoard();
+
+        List<Integer> cellValues = new ArrayList<>();
+        int totalCellValue = 0;
+
+        for (int cell = 0; cell < 9; cell++) {
+            // Cell value is given by the ternary decomposition of board
+            int cellValue = (int) Math.floor(board / Math.pow(3, cell)) % 3;
+            cellValues.add(cellValue);
+            totalCellValue += cellValue;
+        }
+
+        boolean canBePlayed = cellValues.get(move) == 0;
+        if (!canBePlayed) {
+            throw new InvalidMoveException();
+        }
+
+        // First player is 1,then 2
+        int player = totalCellValue % 3 == 0 ? 1 : 2;
+        int newBoard = board + player * (int)Math.pow(3, move);
+
+        game.setBoard(newBoard);
         return gameRepository.save(game);
     }
 }
