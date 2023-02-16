914e9a6d9c65e52b3c60e6144de9d7df6aa0ecde
FilipRazek <65445960+FilipRazek@users.noreply.github.com>
2023-02-16 11:09:41 +0100

    :recycle: Move frontend environment variables to constants

--- frontend/src/client/urlBuilder.js
+++ frontend/src/client/urlBuilder.js
@@ -1,19 +1,19 @@
-export const { href: GAME_URL } = new URL(
-  process.env.REACT_APP_GAME_URL,
-  process.env.REACT_APP_BACKEND_URL
-);
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const RELATIVE_NEW_GAME_URL = "game/new";
+const RELATIVE_GAME_URL = "game";
+const RELATIVE_LOBBY_URL = "game/all-joinable";
+const RELATIVE_JOIN_GAME_URL = "game/join";
+
+export const { href: GAME_URL } = new URL(RELATIVE_GAME_URL, BACKEND_URL);
 
 export const { href: NEW_GAME_URL } = new URL(
-  process.env.REACT_APP_NEW_GAME_URL,
-  process.env.REACT_APP_BACKEND_URL
+  RELATIVE_NEW_GAME_URL,
+  BACKEND_URL
 );
 
-export const { href: LOBBY_URL } = new URL(
-  process.env.REACT_APP_LOBBY_URL,
-  process.env.REACT_APP_BACKEND_URL
-);
+export const { href: LOBBY_URL } = new URL(RELATIVE_LOBBY_URL, BACKEND_URL);
 
 export const { href: JOIN_GAME_URL } = new URL(
-  process.env.REACT_APP_JOIN_GAME_URL,
-  process.env.REACT_APP_BACKEND_URL
+  RELATIVE_JOIN_GAME_URL,
+  BACKEND_URL
 );
